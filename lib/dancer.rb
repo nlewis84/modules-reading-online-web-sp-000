940260b205753de58543817cb884ed3d32e0c60b
Nathan Lewis <n.lewis.perc@gmail.com>
2020-12-04 05:52:30 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,6 +3,8 @@ require_relative './dance_module.rb'
 require_relative 'fancy_dance.rb'
 
 class Dancer
+    include dance_module
+  
     extend FancyDance::ClassMethods
     include FancyDance::InstanceMethods
 end
