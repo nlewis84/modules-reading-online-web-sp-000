d6aa19ce6136c2e3751256cf770623b0e15bb3b2
Nathan Lewis <n.lewis.perc@gmail.com>
2020-12-04 05:57:46 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,12 +2,8 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative 'fancy_dance.rb'
 
-class Kid
+class Dancer
   include Dance
-  extend MetaDancing
-    
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
  
   attr_accessor :name
  
