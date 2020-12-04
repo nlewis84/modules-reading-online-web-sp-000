1d1f45293488a7f7c4a3475a3794951cf55732c4
Nathan Lewis <n.lewis.perc@gmail.com>
2020-12-04 05:54:41 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,5 +3,11 @@ require_relative './dance_module.rb'
 require_relative 'fancy_dance.rb'
 
 class Dancer
-    include Dance
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end
